c75cdba44925e8ec4e30ff0b7b6ca71bcecedc3d
Jiří Zdvomka <zdvomka.j@hotmail.cz>
2019-12-03 11:20:23 +0100

    add maven and lucene packages

---
+++ README.md
@@ -0,0 +1,1 @@
+# Information Retrieval course project
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import org.apache.lucene.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
